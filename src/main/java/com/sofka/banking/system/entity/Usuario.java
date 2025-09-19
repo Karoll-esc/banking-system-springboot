acea77fd557a1054da8d1e99aa1039ec65f4079e
Karoll Marcela Escalante Gazabon <karoll.escalante@sofka.com.co>
2025-09-18 22:58:03 -0500

    Add Usuario entity

---
+++ src/main/java/com/sofka/banking/system/entity/Usuario.java
@@ -0,0 +1,37 @@
+package com.sofka.banking.system.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String cedula;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String apellido;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String telefono;
+}
